2eb4782dbc8b21988ab15bd17afae4c3c90046a8
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2021-04-26 09:16:25 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
-export { default as Work } from "./components/Work";
-
-ReactDOM.render(<App />, document.getElementById("root"));
